e79b838bc701e1fba945e0f159438d37d8f2d1c8
Carlisle Payne <87280475+CPayne617@users.noreply.github.com>
2021-10-17 18:17:56 -0400

    Changes group made
    These are the. changes and addition to the code we made in breakout rooms

--- java/com/example/demo/Person.java
+++ java/com/example/demo/Person.java
@@ -6,78 +6,74 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(value="prototype")
+@Scope(value = "prototype")
 
 public class Person {
-  private int pid;
-  private String pname;
-  private String tech;
-  
- @Autowired
-  private Laptop laptop; 
- 
- @Autowired
- private Address addr;
- 
+	private int pid;
+	private String pname;
+	private String tech;
 
- 
-  public Address getAddr() {
-	return addr;
-}
+	@Autowired
+	private Laptop laptop;
 
+	@Autowired
+	private Address addr;
 
-public void setAddr(Address addr) {
-	this.addr = addr;
-}
+	public Address getAddr() {
+		return addr;
+	}
 
+	public void setAddr(Address addr) {
+		this.addr = addr;
+	}
 
-public Person()
-  {
-	  System.out.println("Person Object created..");
-  }
-  
-  
-  public int getPid() {
-	return pid;
-  }
-  
-  public void setPid(int pid) {
-	
-	this.pid = pid;
-  }
-  public String getPname() {
-	return pname;
-  }
-  public void setPname(String pname) {
-	this.pname = pname;
-  }
-  public String getTech() {
-	return tech;
-  }
-  public void setTech(String tech) {
-	this.tech = tech;
-  }
-	
-  
-public Laptop getLaptop() {
-	return laptop;
-}
+	public Person() {
+		System.out.println("Person Object created..");
+	}
 
+	public int getPid() {
+		return pid;
+	}
 
-public void setLaptop(Laptop laptop) {
-	this.laptop = laptop;
-}
+	public void setPid(int pid) {
 
-public String  showLaptop() {
-	this.laptop.setBrand("Lenovo Think Book");
-	return this.laptop.getBrand();
-}
+		this.pid = pid;
+	}
 
+	public String getPname() {
+		return pname;
+	}
 
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
 
-public void show() {
-	System.out.println("inside person class show method...");
-	//laptop.brandname();
-  }
+	public String getTech() {
+		return tech;
+	}
 
+	public void setTech(String tech) {
+		this.tech = tech;
+	}
+
+	public Laptop getLaptop() {
+		return laptop;
+	}
+
+	public void setLaptop(Laptop laptop) {
+		this.laptop = laptop;
+	}
+
+	public String showLaptop() {
+		this.laptop.setBrand("Lenovo Think Book");
+		return this.laptop.getBrand();
+	}
+
+	public void show() {
+		System.out.println("inside person class show method...");
+		// laptop.brandname();
+	}
+	public void brandname() {
+		System.out.println("This is brandname method from Person class");
+	}
 }
